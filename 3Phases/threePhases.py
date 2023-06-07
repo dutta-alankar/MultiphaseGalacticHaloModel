df1af116227bd5f919b1548aba013723bb6c7f56
Mukesh Singh Bisht <103030464+mukeshrri@users.noreply.github.com>
2023-06-07 10:00:55 +0530

    Update threePhases.py

--- 3Phases/threePhases.py
+++ 3Phases/threePhases.py
@@ -27,20 +27,20 @@ metallicity = (
 
 T = np.logspace(3.0, 8.0, 1000)
 
-f_Vh = 0.938
-f_Vw = 0.060
+f_Vh = 0.937
+f_Vw = 0.061
 f_Vc = 1 - (f_Vh + f_Vw)
 
-sig_u = 0.495
-T_u = 10.0**5.754
+sig_u = 0.494
+T_u = 10.0**5.762
 T_u_M = T_u * np.exp(-(sig_u**2) / 2)
 
 T_h = T_u  # 10.**5.8
-T_w = 10**5.270
-T_c = 10.0**4.102
+T_w = 10**5.261
+T_c = 10.0**4.101
 
-sig_h = 0.495
-sig_w = 1.050
+sig_h = 0.494
+sig_w = 1.001
 sig_c = 0.300
 
 # mid-way between isochoric=1 and isobaric=0;
@@ -59,40 +59,32 @@ np.save(
     "./figures/mcmc_opt-parameters.npy",
     np.array([f_Vh, f_Vw, f_Vc, x_h, x_w, x_c, sig_h, sig_w, sig_c, T_u]),
 )
-
-# del_h = 0.1
-# del_w = 0.8
-# del_c = 3.0
-
-# c_h = 0.1
-# c_w = 0.5
-# c_c = 50000.0
-
+'''
 # Working !
-# del_h = 1.2
-# del_w = 0.3
-# del_c = 3.0
+del_h = 1.2
+del_w = 0.3
+del_c = 3.0
 
-# c_h = 0.13
-# c_w = 0.2
-# c_c = 10.0**4.8
+c_h = -1.13
+c_w = -1.2
+c_c = 11.8
+'''
+del_h = 1.209
+del_w = 0.298
+del_c = 3.063
 
-del_h = 1.55523605
-del_w = 0.05
-del_c = 8.47981696
-
-c_h = 0.57866151
-c_w = 0.1296730
-c_c = 10.0**4.5
+c_h = -1.113
+c_w = -1.197
+c_c = 11.780
 
 
-beta_h = (np.log(c_h) + del_h * (x_h + sig_u**2 / 2 + del_h * sig_h**2 / 2)) / (
+beta_h = (c_h + del_h * (x_h + sig_u**2 / 2 + del_h * sig_h**2 / 2)) / (
     x_h + sig_u**2 / 2 + sig_h**2 * (del_h - 0.5)
 )
-beta_w = (np.log(c_w) + del_w * (x_w + sig_u**2 / 2 + del_w * sig_w**2 / 2)) / (
+beta_w = (c_w + del_w * (x_w + sig_u**2 / 2 + del_w * sig_w**2 / 2)) / (
     x_w + sig_u**2 / 2 + sig_w**2 * (del_w - 0.5)
 )
-beta_c = (np.log(c_c) + del_c * (x_c + sig_u**2 / 2 + del_c * sig_c**2 / 2)) / (
+beta_c = (c_c + del_c * (x_c + sig_u**2 / 2 + del_c * sig_c**2 / 2)) / (
     x_c + sig_u**2 / 2 + sig_c**2 * (del_c - 0.5)
 )
 
@@ -122,19 +114,19 @@ rho_av = f_Vh * rho_av_h + f_Vw * rho_av_w + f_Vc * rho_av_c
 
 M_pdf_m = (
     f_Vh
-    * c_h
+    * np.exp(c_h)
     * np.exp((del_h - 1) * (x_h + (del_h - 1) * sig_h**2 / 2 + sig_u**2 / 2))
     * N_pdf(x, x_h - (1 - del_h) * sig_h * sig_h, sig_h)
 )
 M_pdf_m += (
     f_Vw
-    * c_w
+    * np.exp(c_w)
     * np.exp((del_w - 1) * (x_w + (del_w - 1) * sig_w**2 / 2 + sig_u**2 / 2))
     * N_pdf(x, x_w - (1 - del_w) * sig_w * sig_w, sig_w)
 )
 M_pdf_m += (
     f_Vc
-    * c_c
+    * np.exp(c_c)
     * np.exp((del_c - 1) * (x_c + (del_c - 1) * sig_c**2 / 2 + sig_u**2 / 2))
     * N_pdf(x, x_c - (1 - del_c) * sig_c * sig_c, sig_c)
 )
@@ -142,16 +134,16 @@ M_pdf_m *= rho_av_u / rho_av
 # M_pdf_m /= np.trapz(M_pdf_m,x)
 
 # alternative mass PDF
-M_pdf_alt = c_h * f_Vh * (T / T_u_M) ** (del_h - 1) * N_pdf(x, x_h, sig_h)
-M_pdf_alt += c_w * f_Vw * (T / T_u_M) ** (del_w - 1) * N_pdf(x, x_w, sig_w)
-M_pdf_alt += c_c * f_Vc * (T / T_u_M) ** (del_c - 1) * N_pdf(x, x_c, sig_c)
+M_pdf_alt = np.exp(c_h) * f_Vh * (T / T_u_M) ** (del_h - 1) * N_pdf(x, x_h, sig_h)
+M_pdf_alt += np.exp(c_w) * f_Vw * (T / T_u_M) ** (del_w - 1) * N_pdf(x, x_w, sig_w)
+M_pdf_alt += np.exp(c_c) * f_Vc * (T / T_u_M) ** (del_c - 1) * N_pdf(x, x_c, sig_c)
 M_pdf_alt *= rho_av_u / rho_av
 
 # using exact luminosity function
 hot_lum = (
     np.exp((del_h - 1) * sig_u**2)
     * f_Vh
-    * c_h**2
+    * np.exp(2*c_h)
     * cooling_approx(np.exp(x) * T_u, metallicity)
     * np.exp(2 * (del_h - 1) * (x_h + (del_h - 1) * sig_h * sig_h))
     * N_pdf(x, x_h + 2 * (del_h - 1) * sig_h * sig_h, sig_h)
@@ -160,7 +152,7 @@ L_pdf_m = hot_lum
 warm_lum = (
     np.exp((del_w - 1) * sig_u**2)
     * f_Vw
-    * c_w**2
+    * np.exp(2*c_w)
     * cooling_approx(np.exp(x) * T_u, metallicity)
     * np.exp(2 * (del_w - 1) * (x_w + (del_w - 1) * sig_w * sig_w))
     * N_pdf(x, x_w + 2 * (del_w - 1) * sig_w * sig_w, sig_w)
@@ -169,7 +161,7 @@ L_pdf_m += warm_lum
 cold_lum = (
     np.exp((del_c - 1) * sig_u**2)
     * f_Vc
-    * c_c**2
+    * np.exp(2*c_c)
     * cooling_approx(np.exp(x) * T_u, metallicity)
     * np.exp(2 * (del_c - 1) * (x_c + (del_c - 1) * sig_c * sig_c))
     * N_pdf(x, x_c + 2 * (del_c - 1) * sig_c * sig_c, sig_c)
